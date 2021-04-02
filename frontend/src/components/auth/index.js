d42a457211a72ca0fe434f3b3a8ba22aa5fc4c50
Freddy Caron <freddycaron.1@gmail.com>
2021-04-02 17:55:28 +0200

    add index.js

---
+++ frontend/src/components/auth/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+
+const Auth = ( props ) => {
+  const [signUpChoice, setSignUpChoice] = useState(props.prSignup); /* -(hook)- sur (true), c'est la props du composant Profil */
+  const [loginChoice, setLoginChoice] = useState(props.prLogin); /* sur (false) */
+
+  const selectChoice = (event) => { /* on récupère l'événement (event) cliqué */
+    if (event.target.id === "signup-btn") { /* si l'id ciblé est signup */
+      setLoginChoice(false);
+      setSignUpChoice(true);
+    } else if (event.target.id === "login-btn") {
+      setSignUpChoice(false);
+      setLoginChoice(true);
+    }
+  };
+
+  return (
+    <div className="connection-form">
+      <div className="form-container">
+        <ul>
+          <li id="signup-btn" onClick={selectChoice}
+          className={signUpChoice ? "active-btn" : null}>S'inscrire</li> {/* si signUpForm est sur true (mettre la classe active-btn), sinon (pas de classe) */}
+          <li id="login-btn" onClick={selectChoice}
+          className={loginChoice ? "active-btn" : null}>Se connecter</li>
+        </ul>
+        {signUpChoice && <SignupForm />} {/* si signUpForm est sur true, on affiche la page Signup */}
+        {loginChoice && <LoginForm />} {/* si loginForm est sur true, on affiche la page Login */}
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
