799b133c76536f52eec1649dc43f74f0e82e6c0b
Freddy Caron <freddycaron.1@gmail.com>
2021-04-02 16:26:58 +0200

    add user.js

---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const multer = require('../middleware/multer-config');
+const authCtrl = require('../controllers/auth');
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', authCtrl.signup);
+router.post('/login', authCtrl.login);
+
+// router.put('/:id', multer, userCtrl.modifyUser);
+router.delete('/:id', userCtrl.deleteUser);
+router.get('/', userCtrl.getAllUsers);
+router.get('/:id', userCtrl.getOneUser);
+
+module.exports = router;
