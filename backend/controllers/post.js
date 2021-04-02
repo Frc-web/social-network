379f6241be0deea2f4640a5892f098d2271adb80
Freddy Caron <freddycaron.1@gmail.com>
2021-04-02 16:31:47 +0200

    add post.js

---
+++ backend/controllers/post.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const dbConnect = require('../config/db');
+
+exports.createPost = (req, res, _next) => {
+  let attachmentUrl = "";
+  if (req.file) {
+    attachmentUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+  }
+  dbConnect.query(`INSERT INTO posts (userId, title, content, attachment, date ) VALUES (?,?,?,?, NOW())`, [req.decodToken.userId, req.body.title, req.body.content, attachmentUrl], function (error, results, fields) {
+    if (error) throw error;
+    return res.status(201).json({ message: "Message créé !" })
+  });
+}
+
+exports.modifyPost = (req, res, _next) => {
+  dbConnect.query(`UPDATE posts SET title=?, content=?, attachment=? WHERE id= ?`, [req.body.title, req.body.content, req.body.attachment, req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    return res.status(200).json({ message: 'Objet modifié' })
+  })
+}
+
+exports.deletePost = (req, res, _next) => {
+  dbConnect.query(`DELETE FROM posts WHERE id= ?`, [req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    return res.status(200).json({ message: 'Objet supprimé' })
+  })
+}
+
+exports.getAllPosts = (req, res, _next) => {
+  dbConnect.query(`SELECT * FROM posts`, function (error, results, fields) {
+    if (error) throw error;
+    return res.status(200).json({ results })
+  })
+}
+
+
+exports.getOnePost = (req, res, _next) => {
+  dbConnect.query(`SELECT * FROM posts  WHERE id=?`, [req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    return res.status(200).json({ results })
+  })
+}
