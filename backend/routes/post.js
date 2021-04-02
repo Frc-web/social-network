0a9181f925847c09ef2aa47077cf0d74d3c5903a
Freddy Caron <freddycaron.1@gmail.com>
2021-04-02 16:27:49 +0200

    add post.js

---
+++ backend/routes/post.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const postCtrl = require('../controllers/post');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer, postCtrl.createPost);
+router.put('/:id', auth, multer, postCtrl.modifyPost);
+router.delete('/:id', auth, postCtrl.deletePost);
+router.get('/', auth, postCtrl.getAllPosts);
+router.get('/:id', auth, postCtrl.getOnePost);
+
+module.exports = router;
