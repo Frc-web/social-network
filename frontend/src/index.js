982e57465ccfded52756d92dca57973b2c8e07b5
Freddy Caron <freddycaron.1@gmail.com>
2021-04-08 18:16:23 +0200

    modify

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
-// c'est le fichier le plus haut, il pointe sur root(contient toute l'application)
+// c'est le fichier le plus haut, il pointe sur root (contient toute l'application)
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.scss'
+import './styles/index.scss';
 
 
 ReactDOM.render(
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Hello sur Home !</h1>
+      <h1>Bienvenue sur Home !</h1>
     </div>
   );
 };
