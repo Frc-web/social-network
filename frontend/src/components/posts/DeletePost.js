c73a4b5ad7095b3f6bd08162fba52206110eb7ff
Freddy Caron <freddycaron.1@gmail.com>
2021-06-17 14:24:32 +0200

    modification bouton de suppression des posts

--- frontend/src/components/posts/DeletePost.js
+++ frontend/src/components/posts/DeletePost.js
@@ -5,11 +5,9 @@ const DeletePost = (props) => {
   const userId = sessionStorage.getItem('userId');
   const isAdmin = sessionStorage.getItem('isAdmin');
 
-  if (isAdmin == 1 || userId == props.author)  {
+  if (isAdmin == 1 || userId == props.author || userId == props.sharer)  {
     return (
-      <div>
         <button className="btnSupp" onClick={(e) => {props.handleClick(e, props.postId)}}><i className="fas fa-trash-alt fa-supp"></i></button>
-      </div>
     );
   } else {
     return null;
